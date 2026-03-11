a48535200ff44ede8427c29a97312518cf295249
ubeeiovn <163424961+ubeeiovn@users.noreply.github.com>
2026-03-11 19:33:24 +0700

    ph

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "react-refresh/only-export-components": "off",
+      "react-hooks/purity": "off",
     },
   },
 ];
